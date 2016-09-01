a932745ddce3cb6f9358a3fe6c66bb07f6d8f089
Nick Kent <nickkent88@gmail.com>
2016-08-31 20:09:19 -0600

    Add main()

--- test_pulsetrain.py
+++ test_pulsetrain.py
@@ -1,4 +1,10 @@
 import unittest
 
 class TestPulseMethods(unittest.TestCase):
-    pass
+
+    def test_method1(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
